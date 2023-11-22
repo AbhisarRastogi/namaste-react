baad57a1f737e2b244c03f79823a55414745a033
Abhisar <abhisar1994@gmail.com>
2023-11-22 15:16:38 +0530

    changes in index.html

--- App.js
+++ App.js
@@ -2,41 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Hello World from React"
-);
+    "h1", 
+    {}, 
+    "Namaste React 🚀"
+    );
 
-console.log(heading);   // Object
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// root.render(heading);
-
-// ---------------------------------------------------------------------------------
-
-/**
- * <div id="parent">
- *      <div id="child1">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- *      <div id="child2">
- *          <h1>I'm h1 tag</h1>
- *          <h2>I'm h2 tag</h2>
- *      </div>
- * </div>
- */
-
-
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I'm h1 tag 🚀"),
-        React.createElement("h2", {}, "I'm h2 tag")]),
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag")])
-
-])
-
-root.render(parent);
+root.render(heading);
